2f067b81198a6b66bc5418e68671763f30c78f11
JonyPoter <joangilabert2002@gmail.com>
2020-10-07 09:32:15 +0200

    Nous Fitxers

---
+++ index.php
@@ -0,0 +1,6 @@
+echo'Al gener fa mol de frer';
+echo'Hola muy buenas tardes';
+echo'Hamburguesa golosa el veneno de tu ex';
+echo'Papote Malote con chupa';
+echo'Buenas mis panas';
+echo'Hace frio fuera de casa';
